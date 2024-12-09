45a81b1b18fc4ae2b291685cb89b0038a060a7cf
Lee <155739838+LemaTaposhe@users.noreply.github.com>
2024-12-10 03:50:27 +0600

    Update account.service.ts

--- BankMS_Angular/src/app/account/account.service.ts
+++ BankMS_Angular/src/app/account/account.service.ts
@@ -25,13 +25,15 @@ export class AccountService {
     };
   }
 
+ 
+  // Get all accounts
   getAllAccounts(): Observable<Account[]> {
-    return this.httpClient.get<Account[]>(this.apiUrl, this.getHttpOptions())
+    return this.httpClient.get<any>(this.apiUrl, this.getHttpOptions())
       .pipe(
+        map(response => response.$values),  // Extract the array from the response
         catchError(this.handleError)
       );
   }
-
   getAccountById(id: number): Observable<Account> {
     return this.httpClient.get<Account>(`${this.apiUrl}/${id}`, this.getHttpOptions())
       .pipe(
@@ -39,22 +41,25 @@ export class AccountService {
       );
   }
 
+ 
+  // Apply for a new account
   applyForAccount(account: Account): Observable<Account> {
     return this.httpClient.post<Account>(`${this.apiUrl}/apply`, account, this.getHttpOptions())
       .pipe(
         catchError(this.handleError)
       );
   }
-
+ 
+  // Update an account
   updateAccount(id: number, account: Account): Observable<Account> {
     return this.httpClient.put<Account>(`${this.apiUrl}/${id}`, account, this.getHttpOptions())
       .pipe(
         catchError(this.handleError)
       );
   }
-
+ 
   closeAccount(id: number): Observable<void> {
-    return this.httpClient.delete<void>(`${this.apiUrl}/close/${id}`, this.getHttpOptions())
+    return this.httpClient.delete<void>(`${this.apiUrl}/${id}`, this.getHttpOptions())
       .pipe(
         catchError(this.handleError)
       );
