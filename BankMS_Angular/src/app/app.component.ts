b6dd5f4b839a271623ab399e42eff6dafc72964b
Lee <155739838+LemaTaposhe@users.noreply.github.com>
2024-12-10 03:39:23 +0600

    Update app.component.ts

--- BankMS_Angular/src/app/app.component.ts
+++ BankMS_Angular/src/app/app.component.ts
@@ -1,10 +1,31 @@
+//import { Component } from '@angular/core';
+
+//@Component({
+//  selector: 'app-root',
+//  templateUrl: './app.component.html',
+//  styleUrls: ['./app.component.css']
+//})
+//export class AppComponent {
+//  title = 'BankMS_Angular';
+//}
 import { Component } from '@angular/core';
+import { AuthService } from './auth/auth.service';
+//import { AuthService } from './auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'BankMS_Angular';
+  title = 'Bank Management System';
+
+  constructor(private authService: AuthService) { }
+
+  logout(): void {
+    this.authService.logout();
+  }
+  get isLoggedIn(): boolean {
+    return !!localStorage.getItem('authToken'); // Check if token exists
+  }
 }
