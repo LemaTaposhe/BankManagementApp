47dd3a232a31c56f7d5e95fb289dd122ff103545
Lee <155739838+LemaTaposhe@users.noreply.github.com>
2024-12-10 03:41:51 +0600

    Update app-routing.module.ts

--- BankMS_Angular/src/app/app-routing.module.ts
+++ BankMS_Angular/src/app/app-routing.module.ts
@@ -1,44 +1,4 @@
-//import { NgModule } from '@angular/core';
-//import { RouterModule, Routes } from '@angular/router';
-//import { HomeComponent } from './home/home.component';
-//import { UserComponent } from './user/user.component';
-//import { CommonModule } from '@angular/common';
-//import { AccountComponent } from './account/account.component';
-//import { LoanComponent } from './loan/loan.component';
-//import { TransactionComponent } from './transaction/transaction.component';
 
-//const routes: Routes = [
-//  {
-//    path: 'home', data: { title: 'Home' },
-//    children: [{ path: '', component: HomeComponent }] // Add the Home route
-//  },
-//  {
-//    path: 'user', data: { title: 'User' },
-//    children: [{ path: '', component: UserComponent }]
-//  },
-//  {
-//    path: 'account', data: { title: 'Account' },
-//    children: [{ path: '', component: AccountComponent }]
-//  },
-//  {
-//    path: 'loan', data: { title: 'Loan' },
-//    children: [{ path: '', component: LoanComponent }]
-//  },
-//  {
-//    path: 'transaction', data: { title: 'Transaction' },
-//    children: [{ path: '', component: TransactionComponent }]
-//  },
-
-//  {
-//    path: '', redirectTo: '/home', pathMatch: 'full' // Redirect the default path to Home
-//  }
-//];
-
-//@NgModule({
-//  imports: [CommonModule, RouterModule.forRoot(routes)],
-//  exports: [RouterModule]
-//})
-//export class AppRoutingModule { }
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
@@ -49,29 +9,28 @@ import { LoanComponent } from './loan/loan.component';
 import { TransactionComponent } from './transaction/transaction.component';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
-//import { LoginComponent } from './login/login.component'; // Import LoginComponent
-//import { RegistrationComponent } from './registration/registration.component'; // Import RegistrationComponent
+import { AuthGuard } from './auth/auth.guard';
 
 const routes: Routes = [
   {
     path: 'home', data: { title: 'Home' },
-    children: [{ path: '', component: HomeComponent }]
+    children: [{ path: '', component: HomeComponent  }]
   },
   {
     path: 'user', data: { title: 'User' },
-    children: [{ path: '', component: UserComponent }]
+    children: [{ path: '', component: UserComponent, canActivate: [AuthGuard] }]
   },
   {
     path: 'account', data: { title: 'Account' },
-    children: [{ path: '', component: AccountComponent }]
+    children: [{ path: '', component: AccountComponent, canActivate: [AuthGuard] }]
   },
   {
     path: 'loan', data: { title: 'Loan' },
-    children: [{ path: '', component: LoanComponent }]
+    children: [{ path: '', component: LoanComponent, canActivate: [AuthGuard] }]
   },
   {
     path: 'transaction', data: { title: 'Transaction' },
-    children: [{ path: '', component: TransactionComponent }]
+    children: [{ path: '', component: TransactionComponent, canActivate: [AuthGuard] }]
   },
   {
     path: 'login', data: { title: 'Login' }, // Add Login route
